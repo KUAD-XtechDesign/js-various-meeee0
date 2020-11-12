44e8e0536e4520abca48b32fd9976a7074463588
meeee0 <m-kitamori5271@st.kyoto-art.ac.jp>
2020-11-12 23:33:07 +0900

    copy&arrange_sample02

---
+++ task/js/script_task01.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    let windowH
+    let documentH 
+    let documentW 
+    let scrollTop
+    let separate
+    let scrollRatio
+  
+    $(window).on("scroll resize load",function(){
+      windowH = $(this).height()
+      documentH = $(document).height()
+      documentW = $(document).width()
+      scrollTop = $(this).scrollTop()
+      scrollRatio = scrollTop/(documentH-windowH)
+      
+      separate = documentH / 6;
+  
+      if(scrollTop < separate){
+        $("body").removeClass().addClass("one")
+      }else if(scrollTop < separate * 2){
+        $("body").removeClass().addClass("two")
+      }else{
+        $("body").removeClass().addClass("three")
+      }
+  
+      console.log(windowH,documentH,scrollTop)
+  
+    })
+ 
+  })
